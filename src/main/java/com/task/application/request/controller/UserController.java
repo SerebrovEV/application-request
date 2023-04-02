7c1c25efa0e8a4a7c7278c7a49185074ddb9e6e6
evgen <89818121788@mail.ru>
2023-04-02 12:42:40 +0300

    Made user controller and service

--- src/main/java/com/task/application/request/controller/UserController.java
+++ src/main/java/com/task/application/request/controller/UserController.java
@@ -1,26 +1,32 @@
 package com.task.application.request.controller;
 
-import com.task.application.request.dto.UserDto;
+import com.task.application.request.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
 
     @GetMapping
-    public ResponseEntity<UserDto> getUsers(@RequestParam(required = false) String name,
-                                            Authentication authentication) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity getUsers(@RequestParam(required = false) String name,
+                                                 Authentication authentication) {
+        if(name == null){
+            return ResponseEntity.ok(userService.getAllUser(authentication));
+        }
+        return ResponseEntity.ok(userService.getUserByName(name, authentication));
     }
 
     @PatchMapping("/{userId}")
     public ResponseEntity<Void> setUserStatus(@PathVariable Integer userId,
                                               @RequestParam String status,
                                               Authentication authentication) {
+        userService.setUserStatus(userId, status, authentication);
         return ResponseEntity.ok().build();
     }
 }
